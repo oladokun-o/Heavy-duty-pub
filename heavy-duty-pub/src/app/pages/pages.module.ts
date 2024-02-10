e0ba44e57577c59f59d6cb82c8f2313ca294b63a
Oladokun Oladipupo <115462144+oladokuno@users.noreply.github.com>
2024-02-10 13:11:45 +0100

    update

---
+++ heavy-duty-pub/src/app/pages/pages.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PagesRoutingModule } from './pages-routing.module';
+import { PagesComponent } from './pages.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ServicesComponent } from './services/services.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProductsComponent } from './products/products.component';
+import { NewsComponent } from './news/news.component';
+import { LayoutModule } from '../layout/layout.module';
+
+const components = [
+  HomeComponent,
+  AboutUsComponent,
+  ServicesComponent,
+  ContactComponent,
+  ProductsComponent,
+  NewsComponent
+];
+@NgModule({
+  declarations: [
+    PagesComponent,
+    ...components
+  ],
+  imports: [
+    CommonModule,
+    PagesRoutingModule,
+    RouterModule,
+    LayoutModule
+  ]
+})
+export class PagesModule { }
