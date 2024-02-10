a29dfc59fa4e8553d4c27ed1af071e6c66fb0bd4
Oladokun Oladipupo <ola@5centscdn.com>
2024-02-10 13:21:14 +0100

    update

--- HeavyDutyPub/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-
-export const routes: Routes = [
-  {
-    path: '',
-    loadChildren: () => import('./pages/pages-routing.module').then(Module => Module.PagesRoutingModule)
-  }
-];
